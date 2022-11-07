eb27c96b9c024f47d67167c8b3c1aed16cce497c
demd7362 <demd4784@gmail.com>
2022-11-07 17:40:33 +0900

    짝지어 제거하기
    https://school.programmers.co.kr/learn/courses/30/lessons/12973

---
+++ level2/Solution_09.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution_09
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>(); // "ccbaabaa"
+        for (int i = 0; i < s.length(); i++) {
+            char tmp = s.charAt(i); // tmp에 c 저장
+            if (!stack.isEmpty() && stack.peek() == tmp) { // 스택을 들여다봤을때 tmp면
+                stack.pop(); // 뺀다
+            } else
+                stack.push(tmp); // 들여본 스택이 tmp가 아니면 넣어야
+        }
+        return (stack.isEmpty()) ? 1 : 0;
+    }
+}
