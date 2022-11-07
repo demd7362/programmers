e371fa228be0f7b43e0ed54e0f98702e1bb0d76c
JeongJihun <115058411+demd7362@users.noreply.github.com>
2022-11-07 16:56:08 +0900

    최댓값과 최솟값
    https://school.programmers.co.kr/learn/courses/30/lessons/12939

---
+++ level2/Solution_1.java
@@ -0,0 +1,17 @@
+class Solution_1 {
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+        
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(Integer.parseInt(arr[i]) > Integer.parseInt(arr[j])){
+                    String tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+        return arr[0]+" "+arr[arr.length-1];
+
+    }
+}
