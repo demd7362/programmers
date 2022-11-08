a259c8ef70716611a7ed866658d017c2c2eadd72
JeongJihun <115058411+demd7362@users.noreply.github.com>
2022-11-08 10:08:44 +0900

    Update and rename level2/Solution_12.java to level2/N개의 최소공배수/Solution.java
    https://school.programmers.co.kr/learn/courses/30/lessons/12953

--- level2/N개의 최소공배수/Solution.java
+++ level2/N개의 최소공배수/Solution.java
@@ -1,13 +1,14 @@
 import java.util.*;
-class Solution_12 {
+
+class Solution {
     public int solution(int[] arr) {
         Arrays.sort(arr);
         int answer = 0;
-        for (int i = arr[0],cnt = 0 ; cnt != arr.length; i++) {
+        for (int i = arr[0], cnt = 0; cnt != arr.length; i++) {
             cnt = 0;
             for (int j = 0; j < arr.length; j++) {
-                if ((i * arr[arr.length-1]) % arr[j] == 0) {
-                    answer = i * arr[arr.length-1];
+                if ((i * arr[arr.length - 1]) % arr[j] == 0) {
+                    answer = i * arr[arr.length - 1];
                     cnt++;
                 }
             }
