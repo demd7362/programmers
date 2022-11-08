53c7d2f5502028e8148f6bbfb12442bcae663579
JeongJihun <115058411+demd7362@users.noreply.github.com>
2022-11-08 15:19:21 +0900

    Create Solution.java

---
+++ level1/같은 숫자는 싫어/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i : arr) {
+            if (!stack.isEmpty() && stack.peek() != i) {
+                stack.push(i);
+            } else if (stack.isEmpty()) {
+                stack.push(i);
+            }
+        }
+        int[] answer = new int[stack.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = stack.get(i);
+        }
+        return answer;
+    }
+}
