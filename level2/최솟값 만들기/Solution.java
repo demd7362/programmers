58709f5a8050dca0c9067f5e419ca60eba1dece6
JeongJihun <115058411+demd7362@users.noreply.github.com>
2022-11-08 10:01:03 +0900

    Update and rename level2/Solution_03.java to level2/최솟값 만들기/Solution.java
    https://school.programmers.co.kr/learn/courses/30/lessons/12941

---
+++ level2/최솟값 만들기/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int sum = 0;
+        Integer[] C = new Integer[B.length];
+        for (int i = 0; i < B.length; i++) {
+            C[i] = B[i];
+        }
+        Arrays.sort(A);
+        Arrays.sort(C, Collections.reverseOrder());
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i] * C[i];
+        }
+        return sum;
+    }
+}
