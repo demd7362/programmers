16ec7b50c57510a4b2dd2a9463a256b77acb9b38
JeongJihun <115058411+demd7362@users.noreply.github.com>
2022-11-08 10:07:03 +0900

    Update and rename level2/Solution_10.java to level2/영어 끝말잇기/Solution.java
    https://school.programmers.co.kr/learn/courses/30/lessons/12981

--- level2/영어 끝말잇기/Solution.java
+++ level2/영어 끝말잇기/Solution.java
@@ -1,24 +1,26 @@
 import java.util.*;
-class Solution_10 {
+
+class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
         ArrayList<String> al = new ArrayList<>();
         for (int i = 1; i < words.length; i++) {
-            // 중복감지
+            // 중복 
             al.add(words[i - 1]);
             if (al.contains(words[i])) {
+                System.out.println("중복감지문 i: " + i);
                 answer[0] = (i % n) + 1;
                 answer[1] = (i / n) + 1;
                 break;
             }
-            // 끝말잇기 실패 감지
-            else if (words[i-1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
+            // 끝말잇기 실패 
+            else if (words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
+                System.out.println("틀림감지문 i: " + i);
                 answer[0] = (i % n) + 1;
                 answer[1] = (i / n) + 1;
                 break;
             }
         }
-
         return answer;
     }
 }
