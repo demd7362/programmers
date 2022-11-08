4d0b7e5a79310aceed6f315cb45111985e7e03d6
JeongJihun <115058411+demd7362@users.noreply.github.com>
2022-11-08 10:04:09 +0900

    Update and rename level2/Solution_06.java to level2/숫자의 표현/Solution.java
    https://school.programmers.co.kr/learn/courses/30/lessons/12924

--- level2/숫자의 표현/Solution.java
+++ level2/숫자의 표현/Solution.java
@@ -1,22 +1,22 @@
-class Solution_06 {
+class Solution {
     public int solution(int n) {
         int j = 1;
         int cnt = 1;
-        while(j<n){
+        while (j < n) {
             int sum = 0;
-            for(int i=j; i<=n; i++){
+            for (int i = j; i <= n; i++) {
                 sum += i;
-                if(sum == n) {
+                if (sum == n) {
                     cnt++;
                     j++;
                     sum = 0;
                     break;
-                } else if(sum>n) {
+                } else if (sum > n) {
                     j++;
                     break;
                 }
-            } 
-        }    
+            }
+        }
         return cnt;
     }
 }
