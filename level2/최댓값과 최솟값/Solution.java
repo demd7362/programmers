168724e2a034dc0f543289854202f0f26375332d
JeongJihun <115058411+demd7362@users.noreply.github.com>
2022-11-08 09:55:50 +0900

    Update and rename level2/Solution_01.java to level2/최댓값과 최솟값/Solution.java

---
+++ level2/최댓값과 최솟값/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (Integer.parseInt(arr[i]) > Integer.parseInt(arr[j])) {
+                    String tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+        return arr[0] + " " + arr[arr.length - 1];
+    }
+}
