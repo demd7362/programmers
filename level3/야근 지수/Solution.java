def5832fd9d157efc5c65298f82b401f504a1caf
Jihun Jeong <demd4784@gmail.com>
2023-01-22 19:47:47 +0900

    Update Solution.java

--- level3/야근 지수/Solution.java
+++ level3/야근 지수/Solution.java
@@ -5,6 +5,7 @@ class Solution {
 		long answer = 0;
 		int sum = 0;
 		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+		// 반복문에서 지속적인 정렬이 필요할때 PriorityQueue를 쓰면 시간 복잡도가 줄어든다
 		for (int work : works) {
 			priorityQueue.offer(work);
 			sum += work;
